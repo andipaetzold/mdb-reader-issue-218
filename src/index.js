c8ca47c9e79fe603b0008b9cf3dfd7b9ef570f74
Andi Pätzold <github@andipaetzold.com>
2022-05-25 00:14:16 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import { Buffer } from "buffer/";
+import MDBReader from "mdb-reader";
+
+const button = document.getElementById("button");
+const input = document.getElementById("input");
+const tableNames = document.getElementById("tableNames");
+
+button.addEventListener("click", loadTableNames);
+function loadTableNames() {
+  tableNames.innerHTML = "";
+
+  const files = input.files;
+  if (files.length !== 1) {
+    return;
+  }
+  const file = files[0];
+
+  const reader = new FileReader();
+  reader.onload = (e) => handleBuffer(e.target.result);
+  reader.readAsArrayBuffer(file);
+}
+
+function handleBuffer(buffer) {
+  const reader = new MDBReader(Buffer.from(buffer));
+
+  for (const tableName of reader.getTableNames()) {
+    const table = reader.getTable(tableName);
+    console.log(table.getData());
+  }
+}
